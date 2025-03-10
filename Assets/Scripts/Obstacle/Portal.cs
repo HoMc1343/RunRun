4c6b9ef5cc5f8a1f147dd01a0a79e2ea9b2ac377
HoMc1343 <qre123456@gmail.com>
2025-03-10 11:43:54 +0900

    [fix] 맵, 포탈 수정

--- Assets/Scripts/Obstacle/Portal.cs
+++ Assets/Scripts/Obstacle/Portal.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    public Renderer portalRenderer;
+    private float lightingPortal = 1f; // 발광 강도
+    private bool lighting = true; // 발광
+    private float lightingSpeed = 2f; // 발광 속도
+
     public string targetScene;
     public string portalTag;
     
@@ -15,5 +20,20 @@ public class Portal : MonoBehaviour
             GameManager.Instance.SetDefaultSpawnPosition(targetScene);
             SceneController.Instance.UsePortal(targetScene, GameManager.Instance.spawnPosition, portalTag);
         }
+    }
+
+
+
+    void Update()
+    {
+        if (lighting)
+            lightingPortal += Time.deltaTime * lightingSpeed; // 증가
+        else
+            lightingPortal -= Time.deltaTime * lightingSpeed; // 감소
+
+        if (lightingPortal >= 1.5f) lighting = false; // 1.5 도달 시 감소
+        else if (lightingPortal <= 0.1f) lighting = true; // 0.1 도달 시 증가
+
+        portalRenderer.material.SetColor("_EmissionColor", Color.white * lightingPortal); // 녹색 발광
     }
 }
